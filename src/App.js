2b78fc5af9a192eb21e8268e28df5c2131b9afe1
areeb <areeb.malik@cooperativecomputing.com>
2022-12-08 13:29:12 +0500

    Added bootstrap CDN

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import bootstrap from 'bootstrap'
 
 function App() {
 
@@ -6,24 +7,7 @@ function App() {
 
   return (
     <>
-      <h1 className="main-head">{header}</h1>
-
-      <div className="navbar-div">
-        <nav className="navbar">
-          <ul className="unordered-list">
-            <li className="list-item">Home</li>
-            <li className="list-item">About</li>
-            <li className="list-item">Login</li>
-            <li className="list-item">Register</li>
-          </ul>
-        </nav>
-      </div>
-
-      <div className="text">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, expedita doloribus? Voluptatibus animi atque explicabo, eveniet minima sunt quam doloribus ipsam praesentium tenetur sapiente fugit cumque autem dolorem, commodi blanditiis dolorum tempore neque tempora omnis ipsa, dignissimos ipsum veniam placeat. Repellendus itaque minus dolorum at vero nulla cupiditate consectetur eaque?
-        </p>
-      </div>
+      
     </>
   );
 }
